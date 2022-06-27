e449b602c1b6a288e2fc2c213d6f80361c075702
guilherme-ribeiro27 <gui062702@gmail.com>
2022-06-27 19:41:33 -0300

    AdminJs added and configured

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
+import {adminjs, adminJSRouter} from './adminjs/index';
 import express from 'express';
+import { sequelize } from './database';
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(adminjs.options.rootPath, adminJSRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
+    sequelize.authenticate().then(()=>{
+        console.log('Connection has been established successfully.');
+    })
     console.log(`Server is running on port ${PORT}`);
 })
