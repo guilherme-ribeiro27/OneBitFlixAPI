0788c4b3acd767a89b1d305028f126a0108a1c69
guilherme-ribeiro27 <gui062702@gmail.com>
2022-06-28 20:25:27 -0300

    First model set

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -2,6 +2,7 @@ import AdminJS from "adminjs";
 import AdminJSExpress from '@adminjs/express';
 import AdminJSSequelize from '@adminjs/sequelize';
 import { sequelize } from "../database";
+import { adminJsResources } from "./resources";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
@@ -12,23 +13,24 @@ export const adminjs = new AdminJS({
         companyName: 'OneBitFlix',
         logo: '/onebitflix.svg',
         theme: {
-        colors: {
-            primary100: '#ff0043',
-            primary80: '#ff1a57',
-            primary60: '#ff3369',
-            primary40: '#ff4d7c',
-                primary20: '#ff668f',
-            grey100: '#151515',
-            grey80: '#333333',
-            grey60: '#4d4d4d',
-            grey40: '#666666',
-            grey20: '#dddddd',
-            filterBg: '#333333',
-            accent: '#151515',
-            hoverBg: '#151515',
-        }
-        }
-    }
+            colors: {
+                primary100: '#ff0043',
+                primary80: '#ff1a57',
+                primary60: '#ff3369',
+                primary40: '#ff4d7c',
+                    primary20: '#ff668f',
+                grey100: '#151515',
+                grey80: '#333333',
+                grey60: '#4d4d4d',
+                grey40: '#666666',
+                grey20: '#dddddd',
+                filterBg: '#333333',
+                accent: '#151515',
+                hoverBg: '#151515',
+            }
+        },
+    },
+    resources: adminJsResources,
 })
 
 export const adminJSRouter = AdminJSExpress.buildRouter(adminjs);
