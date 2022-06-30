ef65c935bf991226dd2b2e527539c4910587536e
guilherme-ribeiro27 <gui062702@gmail.com>
2022-06-30 09:34:10 -0300

    Model, Migration and resource of Episodes created

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,12 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 Category.hasMany(Course,)
 Course.belongsTo(Category,)
 
+Course.hasMany(Episode)
+Episode.belongsTo(Course)
 export{
-    Category,Course
+    Category,Course,Episode
 }
