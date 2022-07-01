f0f9e37a466a4a256c19e014fcaf319d85308880
guilherme-ribeiro27 <gui062702@gmail.com>
2022-06-30 21:26:12 -0300

    Upload feature added

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 
 export const episodeResourceOptions: ResourceOptions = {
     navigation: 'Catálogo',
@@ -6,4 +8,21 @@ export const episodeResourceOptions: ResourceOptions = {
     filterProperties: ['name', 'synopsis', 'courseId', 'secondsLong', 'createdAt', 'updatedAt'],
     listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong'],
     showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'secondsLong', 'createdAt', 'updatedAt']
+
+
 }
+
+export const episodeResourceFeatures : FeatureType[] = [
+    uploadFileFeature({
+        provider:{
+            local:{
+                bucket: path.join(__dirname, '..','..','..','uploads')
+            }
+        },
+        properties:{
+            key: 'videoUrl',//database column name
+            file: 'uploadVideo'
+        },
+        uploadPath: (record,filename) => `videos/course-${record.get('courseId')}/${filename}`
+    })
+]
