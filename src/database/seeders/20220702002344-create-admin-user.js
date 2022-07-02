d34171ac53ba33345ccfafaa1b9ee52bcd7bb2f1
guilherme-ribeiro27 <gui062702@gmail.com>
2022-07-02 08:21:41 -0300

    Seeder for user

--- src/database/seeders/20220702002344-create-admin-user.js
+++ src/database/seeders/20220702002344-create-admin-user.js
@@ -1,8 +1,22 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
 module.exports = {
   async up (queryInterface, Sequelize) {
     
+    await queryInterface.bulkInsert('users',[
+      {
+        first_name: 'Guilherme',
+        last_name: 'Ribeiro',
+        phone: '54315262',
+        birth: '1992-07-02',
+        email: 'guilherme.ribeiro062002@gmail.com',
+        password: await bcrypt.hash('123456', 10),
+        role:'admin',
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
