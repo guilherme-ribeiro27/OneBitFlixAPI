4d7f18e2fa54bb11ca3720ac95418a84d51e17ca
guilherme-ribeiro27 <gui062702@gmail.com>
2022-07-01 21:28:59 -0300

    Model, Migration and Resource for User created

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
+import { User } from "./User";
 
 Category.hasMany(Course,)
 Course.belongsTo(Category,)
@@ -8,5 +9,5 @@ Course.belongsTo(Category,)
 Course.hasMany(Episode)
 Episode.belongsTo(Course)
 export{
-    Category,Course,Episode
+    Category,Course,Episode,User
 }
